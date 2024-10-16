18611a0e64a3954806a054ba8a355bbd307e2124
JhonAlmanzar <159461041+JhonAlmanzar@users.noreply.github.com>
2024-10-16 17:23:35 -0500

    Categoria y Plato

--- Restaurante/src/main/java/com/example/Restaurante/Model/Plato.java
+++ Restaurante/src/main/java/com/example/Restaurante/Model/Plato.java
@@ -1,6 +1,5 @@
 package com.example.Restaurante.Model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,13 +11,26 @@ import jakarta.persistence.*;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "Categoria")
-public class Categoria {
+@Table(name = "Plato")
+public class Plato {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "idCategoria")
-    private long idCategoria;
+    @Column(name = "idPlato")
+    private long idPlato;
 
     @Column(name = "nombre")
     private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "precio")
+    private long precio;
+
+    @Column(name = "imagen")
+    private String imagen;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria")
+    private Categoria categoria;
 }
