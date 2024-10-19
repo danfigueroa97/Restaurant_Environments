237243742e2e49f6198656bf079cdf99b2ec3872
JhonAlmanzar <159461041+JhonAlmanzar@users.noreply.github.com>
2024-10-18 22:45:26 -0500

    .

--- Restaurante/src/main/java/com/example/Restaurante/Repository/PlatoRepository.java
+++ Restaurante/src/main/java/com/example/Restaurante/Repository/PlatoRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PlatoRepository extends JpaRepository<Plato, Long> {
-
   List<Plato> findByIdPlatoIn(List<Long> idPlato);
 }
