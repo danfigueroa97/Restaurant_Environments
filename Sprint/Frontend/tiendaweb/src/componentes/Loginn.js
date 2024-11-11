6f658db83dfb97ca52f452a037cc21f1ea9336c9
JhonAlmanzar <159461041+JhonAlmanzar@users.noreply.github.com>
2024-11-10 23:57:44 -0500

    Front

---
+++ Sprint/Frontend/tiendaweb/src/componentes/Loginn.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import '../estilos/Loginn.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para el inicio de sesión
+    console.log("Email:", email, "Password:", password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Iniciar Sesión</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <label htmlFor="email">Correo Electrónico</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Ingrese su correo"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="password">Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Ingrese su contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="btn-submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
